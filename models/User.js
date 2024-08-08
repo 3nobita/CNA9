3c28cf1f9d96340f5d2f1ab68f8cc96ad981e3cd
Bhavesh Shinde <154302190+3nobita@users.noreply.github.com>
2024-08-08 21:33:37 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,13 @@
+// models/User.js 
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userId: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true, enum: ['admin', 'hod', 'driver', 'employee'] },
+    department: { type: String, required: true }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
